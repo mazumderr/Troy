9878c44cdf348efe847eef031cbd09e13b8052dc
Bo Thompson <bo_l_thompson@yahoo.com>
2024-08-30 10:36:18 -0700

    main handles cli arguments in a useful way

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main(int argc, char** argv){
-  cout << "Waddup" << endl;
+
+  if (argc < 2) {
+    cout << "usage: " << argv[0] << " inputfile" << endl;
+    return 0;
+  }
+
+  //okay now assume that argv[1] is the desired input filename :D
 
   return 0;
 }
