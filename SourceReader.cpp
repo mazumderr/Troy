6707a561f23539bcbaa17134fed4d309758c1f54
mazumderr <135399797+mazumderr@users.noreply.github.com>
2024-09-01 13:18:21 -0700

    Add files via upload
    SourceReader and StringParser

--- SourceReader.cpp
+++ SourceReader.cpp
@@ -1,38 +1,96 @@
+//
+// Created by Rakeen Mazumder, Joshua Lozano on 8/29/24.
+//
+
 #include "SourceReader.h"
-#include <iostream>
-
-/**
- * @brief Construct a new Source Reader:: Source Reader object
- * 
- * @pre input file must exist or an error is triggered
- * @param fname filename to read source code from
- */
-SourceReader::SourceReader(const string &fname) {
-  sourceFile.open(fname);
-
-  if (!sourceFile.is_open()) {
-    error("File \"" + fname + "\" does not exist.");
-  }
+// constructor
+SourceReader::SourceReader(const std::string &filename) {
+    sourceFile = filename;
+    inputStream.open(sourceFile);
 }
 
-/**
- * @brief (PLACEHOLDER) Get a line of source code, comments excluded
- * 
- * @pre c-style block comments must be correctly formatted or an error will be triggered
- * @param output a single line of commentless code
- * @return true if there's more to read
- * @return false if we're EOF
- */
-bool SourceReader::getline(string &output){
-  return false;
-}
+bool SourceReader::processSource(char &c) {
 
-/**
- * @brief Prints and error and ends the program
- * 
- * @param msg error to print
- */
-void SourceReader::error(string msg) {
-  cout << msg << endl;
-  exit(-1);
+    //inputStream.open(sourceFile,std::ios::in);
+    // Make sure file exists
+    if (!inputStream.is_open()) {
+        std::cerr << "File not found!" << std::endl;
+        return false;
+    }
+    // Read file character by character
+    inputStream.get(c);
+    if (inputStream.eof()) {
+        if (mode == modes::block) {
+            std::cerr << "ERROR: Program contains C-style, unterminated comment on line " << begin_comment;
+            inputStream.close();
+            exit(1);
+        }
+        if (stringParser.parse(c)) {                // unterminated string error
+            std::cerr << "ERROR: Program contains unterminated string!";
+            inputStream.close();
+            exit(1);
+        }
+        inputStream.close();
+        return false;
+    }
+
+    //inputStream.get(c);
+    // check for strings
+    /**/
+    if ((mode == modes::normal) && (stringParser.parse(c))) { // string mode
+        return true;
+    }
+
+
+    if (c == '/') {                                // comment found
+        if (inputStream.peek() == '/') {          // single comment state
+            mode = modes::single;
+        }
+        else if ((inputStream.peek() == '*') && mode != modes::single) {
+            mode = modes::block;                   // comment block state
+            begin_comment = line_count;
+        }
+    }
+    if (c == '*') {
+        if (inputStream.peek() == '/') {            // terminate comment block, switch modes
+            if (mode == modes::normal) {
+                std::cerr << "ERROR: Program contains C-style, unterminated comment on line " << line_count;
+                inputStream.close();
+                exit(1);
+            }
+            //c = ' ';
+            if (mode == modes::block) {              // mode switch when reaching end of comment block
+                mode = modes::eocb;
+                c = ' ';
+                return true;
+            }
+        }
+        //return true;
+    }
+    if (c == '\n') {                               // newline
+        line_count++;
+        if (mode == modes::single) {
+            mode = modes::normal;
+            return true;
+        }
+        if (mode == modes::block) {
+            return true;
+        }
+    }
+    if ((mode == modes::single) || (mode == modes::block)) {          // single and block comment
+        c = ' ';
+        return true;
+    }
+    if (mode == modes::eocb) {
+        c = ' ';
+        mode = modes::normal;
+    }
+    /*
+    if (mode == modes::block) {                     // block comment
+        c = ' ';
+        return true;
+    }
+    */
+
+    return true;                                    // source code
 }
