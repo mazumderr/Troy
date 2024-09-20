bca4aa0039edcf4ff4b8390f43a372f0601095c7
mazumderr <135399797+mazumderr@users.noreply.github.com>
2024-09-19 22:15:52 -0700

    Bug fix for stringparser
    Bug fix for Issue #23

--- StringParser.cpp
+++ StringParser.cpp
@@ -7,36 +7,54 @@
 StringParser::StringParser() {}
 
 bool StringParser::parse(char &c) {
-    // single quote
-    if ((c == '\'') && (mode == modes::nonstring)) {
-        mode = modes::string_single;
-        return true;
-    }
-    if ((c == '\'') && (mode == modes::string_single)) {
-        mode = modes::nonstring;
-        return false;
-    }
-    if ((c == '\"') && (mode == modes::nonstring)) {
-        mode = modes::string_double;
-        return true;
-    }
-    if ((c == '\"') && (mode == modes::string_double)) {
-        mode = modes::nonstring;
-        return false;
-    }
+    switch(c) {
+        case '\'':
+            // code block
+            if (mode == modes::nonstring) {
+                mode = modes::string_single;
+                return true;
+            }
+            if (mode == modes::string_single) {
+                mode = modes::nonstring;
+                return false;
+            }
+            if (mode == modes::string_double) {
+                return true;
+            }
+            if (mode == modes::escape) {
+                mode = prev_mode;
+                return true;
+            }
 
-    if ((mode == modes::string_single) || (mode == modes::string_double)) {
-        return true;
-    }
-    if (c == '\\') {
-        prev_mode = mode;
-        mode = modes::escape;
-        return false;
-    }
-    if ((mode == modes::escape) && (c == '\'' || c == '\"')) { // escape character bug
-        mode = prev_mode;
-        return false;
-    }
+        case '\"':
+            // code block
+            if (mode == modes::nonstring) {
+                mode = modes::string_double;
+                return true;
+            }
+            if (mode == modes::string_double) {
+                mode = modes::nonstring;
+                return false;
+            }
+            if (mode == modes::string_single) {
+                return true;
+            }
+            if (mode == modes::escape) {
+                mode = prev_mode;
+                return true;
+            }
 
-    return false;
+        case '\\':
+            // code block
+            prev_mode = mode;
+            mode = modes::escape;
+            return true;
+
+        default:
+            if (mode == modes::nonstring)
+                return false;
+            if (mode == modes::escape)
+                mode = prev_mode;
+            return true;
+    }
 }
