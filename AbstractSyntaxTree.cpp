4a765407bcb416dec39be296747117aaca3af3dc
Andrew <andrew.sc.porter@gmail.com>
2024-10-29 14:54:20 -0700

    add files for Abstract Syntax Tree class

---
+++ AbstractSyntaxTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andrew on 10/29/2024.
+//
+
+#include "AbstractSyntaxTree.hpp"
---
+++ AbstractSyntaxTree.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Andrew on 10/29/2024.
+//
+
+#ifndef TROY_ABSTRACTSYNTAXTREE_HPP
+#define TROY_ABSTRACTSYNTAXTREE_HPP
+
+
+class AbstractSyntaxTree {
+
+};
+
+
+#endif //TROY_ABSTRACTSYNTAXTREE_HPP
