307fd280337b38eddfe629847beff354ff9355b4
mazumderr <135399797+mazumderr@users.noreply.github.com>
2024-12-15 19:07:04 -0800

    output files for assignment 6

---
+++ tests/Interpreter/TestInterpreter.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+cd ../../
+make
+fileout="programming_assignment_6-test_file_"
+mkdir temp/
+for i in `seq 1 3`;
+do
+   ./a.exe io/in/assignment6/programming_assignment_6-test_file_$i.c > "temp/${fileout}${i}.c";
+done
+
+for i in `seq 1 3`;
+do 
+   diff "io/out/assignment6/${i}out.txt" "temp/programming_assignment_6-test_file_${i}.c";
+   if [ $? != 0 ]; then
+      exit 255
+   fi
+done
+rm -rf temp/*
+rmdir temp/
+make clean
